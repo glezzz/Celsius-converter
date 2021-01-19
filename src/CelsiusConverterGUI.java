b96d5884a7c428fe0d994cc68f850d379352883d
glezzz <alexglezk@gmail.com>
2021-01-19 13:42:02 +0100

    panel created

--- src/CelsiusConverterGUI.java
+++ src/CelsiusConverterGUI.java
@@ -1,2 +1,20 @@
-public class CelsiusConverterGUI {
+import javax.swing.*;
+
+public class CelsiusConverterGUI extends JFrame{
+    private JPanel mainPanel;
+
+    public CelsiusConverterGUI(String title) {
+        super(title);
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //content that goes in form
+        this.setContentPane(mainPanel);
+        this.pack();
+
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new CelsiusConverterGUI("Celsius Converter");
+        frame.setVisible(true);
+    }
 }
